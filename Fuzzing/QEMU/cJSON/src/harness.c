d7db3e4ba6cef7420751fc2456c32754160ea3ce
Sergio García <olasergiolas@gmail.com>
2022-04-12 21:43:10 +0200

    Add harness and corpus for cJSON #2
    This commit adds a fuzzing harness and corpus to allow fuzzing of the cJSON .so file located in Wyze Cam V3's firmware (version 4.36.8.26, cJSON version 1.7.13). This library was compiled for the mipsel architecture thus this fuzzing setup being created for fuzzing cross-architecture binaries with AFL++ QEMU mode.
    
    The JSON files that make up the corpus are the ones provided in cJSON's official [repository](https://github.com/DaveGamble/cJSON).

---
+++ Fuzzing/QEMU/cJSON/src/harness.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "cJSON.h"
+#include "cJSON_Utils.h"
+#include <sys/stat.h>
+
+char* read_input(char* path){
+    FILE *f;
+    struct stat st;
+    char* content = NULL;
+    size_t read_elements = 0;
+
+    f = fopen(path, "rb");
+    if (f == NULL)
+        exit(1);
+
+    stat(path, &st);
+    content = (char*)malloc(st.st_size);
+    if (fread(content, st.st_size, 1, f) != 1)
+        exit(1);
+    
+    return content;
+}
+
+int main(int argc, char** argv){
+    char* res = NULL;
+    char* content = NULL;
+
+    if (argc != 2){
+        printf("Usage: %s [path-to-json-file]", argv[0]);
+        exit(1);
+    }
+    
+    content = read_input(argv[1]);
+
+    cJSON *json = cJSON_Parse(content);
+    res = cJSON_Print(json);
+    printf("%s", res);
+}
