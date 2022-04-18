d95161ffa36bf1f78d69bb88ccbd1bf2d29e40c4
Sergio García <olasergiolas@gmail.com>
2022-04-18 21:00:50 +0200

    Fuzz more cJSON functions #2
    Improve fuzzing harness by adding more functions and by skipping unnecessary printf calls.

--- Fuzzing/QEMU/cJSON/src/harness.c
+++ Fuzzing/QEMU/cJSON/src/harness.c
@@ -3,6 +3,7 @@
 #include "cJSON.h"
 #include "cJSON_Utils.h"
 #include <sys/stat.h>
+#include <string.h>
 
 char* read_input(char* path){
     FILE *f;
@@ -16,25 +17,47 @@ char* read_input(char* path){
 
     stat(path, &st);
     content = (char*)malloc(st.st_size);
-    if (fread(content, st.st_size, 1, f) != 1)
+    if (fread(content, st.st_size, 1, f) != 1 || strlen(content) == 0)
         exit(1);
     
     return content;
 }
 
 int main(int argc, char** argv){
-    char* res = NULL;
+    char* print = NULL;
+    char* print_buf = NULL;
+    char* print_buf_fmt = NULL;
     char* content = NULL;
+    char* content_min = NULL;
+    unsigned content_size = 0;
 
-    if (argc != 2){
+    if (argc < 2 || argc > 3){
         printf("Usage: %s [path-to-json-file]", argv[0]);
         exit(1);
     }
     
     content = read_input(argv[1]);
+    content_size = strlen(content);
 
-    cJSON_Minify(content);
+    content_min = (char*)malloc(strlen(content));
+    memcpy(content_min, content, content_size);
+    cJSON_Minify(content_min);
+
     cJSON *json = cJSON_Parse(content);
-    res = cJSON_Print(json);
-    printf("%s", res);
+
+    print = cJSON_Print(json);
+    print_buf = cJSON_PrintBuffered(json, 1, 0);
+    print_buf_fmt = cJSON_PrintBuffered(json, 1, 1);
+
+    if (argc == 3 && strcmp(argv[2], "-v") == 0){
+        printf("cJSON_Print method:\n%s\n", print);
+        printf("cJSON_PrintBuffered method:\n%s\n", print_buf);
+        printf("cJSON_PrintBuffered fmt method:\n%s\n", print_buf_fmt);
+    }
+
+    cJSON_free(json);
+    free(print);
+    free(print_buf);
+    free(print_buf_fmt);
+    free(content_min);
 }
