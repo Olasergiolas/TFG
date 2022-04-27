b2600272ebb01b23e02b8247d02e1c5f9ffaa52c
Sergio García <olasergiolas@gmail.com>
2022-04-27 20:49:52 +0200

    Improve emulation script #6
    Improve usability by making use of argparse. Also allow to load firmware from file.

--- Fuzzing/Qiling/netgear/src/emulate.py
+++ Fuzzing/Qiling/netgear/src/emulate.py
@@ -1,8 +1,10 @@
-import sys
+import sys, argparse
 from qiling import Qiling
 
 TARGET_FUNC_ADDR    = 0x3322c   # Address of the function we are interested in
+TARGET_END_ADDR     = 0x3330c   # End fuzzing when reaching this address
 LIBC_START_ADDR     = 0x0c460   # Address where __libc_start_main is being called
+PAYLOAD             = b"*#$^"
 
 def dumpContext(ql: Qiling):
     print("\nr0: 0x%x" % ql.reg.r0)
@@ -15,17 +17,20 @@ def dumpContext(ql: Qiling):
 def libc_start_main_redirect(ql: Qiling, func_addr = TARGET_FUNC_ADDR):
     ql.reg.write("r0", func_addr)
     
-def indirect_write_reg_string(ql: Qiling, register_id, new_str):
-    address = ql.mem.map_anywhere(len(new_str))
-    ql.mem.string(address, new_str)
+def indirect_write_reg_bytes(ql: Qiling, register_id, bytes):
+    address = ql.mem.map_anywhere(len(bytes))
+    ql.mem.write(address, bytes)
     ql.reg.write(register_id, address) # == (ql.reg.r0 = address) 
     
-def target_hook(ql: Qiling):
-    payload = "*#$^"
+def readPayload(path):
+    with open(path, 'rb') as f:
+        return f.read()
     
+def target_hook(ql: Qiling):    
     print("\n--------------\n")
     print("HOOK, PC: 0x%x" % ql.reg.arch_pc)
-    indirect_write_reg_string(ql, "r0", payload)
+    print("Using", PAYLOAD, "as payload\n")
+    indirect_write_reg_bytes(ql, "r0", PAYLOAD)
     print("\n--------------\n")
     
 def sandbox(path, rootfs, debug):    
@@ -33,14 +38,18 @@ def sandbox(path, rootfs, debug):
     ql.hook_address(libc_start_main_redirect, LIBC_START_ADDR)
     ql.hook_address(target_hook, TARGET_FUNC_ADDR)     
     ql.debugger = debug
-    ql.run(end=0x3330c)
+    ql.run(end=TARGET_END_ADDR)
 
 if __name__ == "__main__":
-    debug = False
-    if len(sys.argv) > 1 and sys.argv[1] == "--gdb":
-        debug = True
+    parser = argparse.ArgumentParser(description="Emulate firmware header check function")
+    parser.add_argument("-f", "--firmware", help="Firmware in .chk format", nargs=1)
+    parser.add_argument("--gdb", help="Use gdb debugger", action="store_true")
+    args = parser.parse_args()
+    
+    if args.firmware:
+        PAYLOAD = readPayload(args.firmware[0])
     
     path = ["/home/sgarcia/TFG/Fuzzing/Qiling/netgear/bin/upnpd"]
     rootfs = "/home/sgarcia/TFG/Firmware/netgear/R7000/squashfs-root"
-    sandbox(path, rootfs, debug)
+    sandbox(path, rootfs, args.gdb)
     
