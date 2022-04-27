7e62f7ea73c8337e5e56f1deb3c02d70915f2462
Sergio García <olasergiolas@gmail.com>
2022-04-27 21:03:39 +0200

    Start debugging fuzzing harness issues #7
    Begin debugging process.

--- Fuzzing/Qiling/netgear/src/fuzz.py
+++ Fuzzing/Qiling/netgear/src/fuzz.py
@@ -6,6 +6,11 @@ TARGET_FUNC_ADDR    = 0x3322c   # Address of the function we are interested in
 TARGET_END_ADDR     = 0x3330c   # End fuzzing when reaching this address
 LIBC_START_ADDR     = 0x0c460   # Address where __libc_start_main is being called
 
+def testhook(ql: Qiling):
+    print("\n\n TEST HOOK \n\n")
+    #ql.reg.write("r2", 0x800)
+    dumpContext(ql)
+
 def dumpContext(ql: Qiling):
     print("\nr0: 0x%x" % ql.reg.r0)
     print("\nr1: 0x%x" % ql.reg.r1)
@@ -17,19 +22,24 @@ def dumpContext(ql: Qiling):
 def libc_start_main_redirect(ql: Qiling, func_addr = TARGET_FUNC_ADDR):
     ql.reg.write("r0", func_addr)
     
-def indirect_write_reg_string(ql: Qiling, register_id, new_str):
-    address = ql.mem.map_anywhere(len(new_str))
-    ql.mem.string(address, new_str)
-    ql.reg.write(register_id, address) # == (ql.reg.r0 = address) 
-    
 def sandbox(path, rootfs, debug, param_file):    
     ql = Qiling(path, rootfs)
     ql.hook_address(libc_start_main_redirect, LIBC_START_ADDR)
+    ql.hook_address(testhook, 0x332d8)
     
     def place_input_callback(_ql: Qiling, input: bytes, _):
         address = _ql.mem.map_anywhere(len(input))
-        _ql.mem.write(address, input)
+        
+        print("\n\n FIRMWARE LENGTH (bytes): \n\n", len(input))
+        
+        #_ql.mem.write(address, input)
+        #_ql.reg.write("r0", address)
+        _ql.uc.mem_write(address, input)
         _ql.reg.write("r0", address)
+        
+        res = _ql.mem.read(address, len(input));
+        print("\n\n FUNCTION INPUT BYTES: \n\n", input)
+        print("\n\n BYTES STORED IN MEMORY: \n\n", res)
         
     def start_afl(_ql: Qiling):
         ql_afl_fuzz(_ql, param_file, place_input_callback, exits=[TARGET_END_ADDR])
